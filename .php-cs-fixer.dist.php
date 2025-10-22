2c0afd990f89226684b2369d4d26e5a2a160f944
MakarMS <makar.mikhalchenko@bk.ru>
2025-10-22 17:19:12 +0300

    chore: Update workflows

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->exclude('vendor');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_extra_blank_lines' => ['tokens' => ['extra']],
+        'blank_line_after_namespace' => true,
+        'single_quote' => true,
+    ])
+    ->setFinder($finder);
